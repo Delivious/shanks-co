8bdfd7483ffebe5b7a5e4ff4ddc913266987cb63
Bobusslob <bader.ori@gmail.com>
2026-04-28 07:59:22 -0400

    arorws

---
+++ MainWeb/games/Fnf/fnf.js
@@ -0,0 +1,4 @@
+left=document.querySelector('#stationarrowleft')
+down=document.querySelector('#stationarrowdown')
+up=document.querySelector('#stationarrowup')
+right=document.querySelector('#stationarrowright')
